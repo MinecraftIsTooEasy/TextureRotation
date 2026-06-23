5edc1e723529b3387c28cc387ab11a178d43f054
vb1dra <vaceslavkozlov732@gmail.com>
2026-06-23 14:27:09 +0300

    mod is client side only now

--- src/main/java/vbonedra/texture_rotation/TextureRotationMod.java
+++ src/main/java/vbonedra/texture_rotation/TextureRotationMod.java
@@ -1,15 +1,15 @@
 package vbonedra.texture_rotation;
 
 import fi.dy.masa.malilib.config.ConfigManager;
+import net.fabricmc.api.ClientModInitializer;
 import vbonedra.texture_rotation.event.TextureRotationEvent;
-import net.fabricmc.api.ModInitializer;
 import net.xiaoyu233.fml.ModResourceManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static vbonedra.texture_rotation.TRConfigs.reinitializeBlockMap;
 
-public class TextureRotationMod implements ModInitializer {
+public class TextureRotationMod implements ClientModInitializer {
     public static final String MOD_ID = "texture_rotation";
     public static final String MOD_NAME = "TextureRotation";
     public static final String RESOURCE_ID = MOD_ID + ":";
@@ -17,7 +17,7 @@ public class TextureRotationMod implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
 //        ModResourceManager.addResourcePackDomain(MOD_ID);
 //        ModResourceManager.addResourcePackDomain("MITE");
         ModResourceManager.addResourcePackDomain("minecraft");
